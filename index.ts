9f69eec04184aa580c572f390efbe4a8bc61b3e4
Gijs <s121866@ap.be>
2024-04-17 09:56:58 +0000

    html paginas naar ejs veranderd

--- index.ts
+++ index.ts
@@ -1,12 +1,47 @@
 import express from "express";
-
 const app = express();
 
+app.set("view engine", "ejs"); // EJS als view engine
 app.set("port", 3000);
 
 app.get("/", (req, res) => {
   res.type("text/html");
-  res.send("Hello <strong>World</strong>");
+  res.render("index");
+});
+
+app.get("/homepage", (req, res) => {
+  res.type("text/html");
+  res.render("homepage");
+});
+
+app.get("/rounds", (req, res) => {
+  res.type("text/html");
+  res.render("rounds");
+});
+
+app.get("/suddendeath", (req, res) => {
+  res.type("text/html");
+  res.render("suddendeath");
+});
+
+app.get("/favourites", (req, res) => {
+  res.type("text/html");
+  res.render("favourites");
+});
+
+app.get("/blacklist", (req, res) => {
+  res.type("text/html");
+  res.render("blacklist");
+});
+
+app.get("/login", (req, res) => {
+  res.type("text/html");
+  res.render("login");
+});
+
+app.get("/registreer", (req, res) => {
+  res.type("text/html");
+  res.render("registreer");
 });
 
 app.listen(app.get("port"), () =>
